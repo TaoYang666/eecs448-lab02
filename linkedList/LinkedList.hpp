bda391d32772580665e153b790eebcd40a80460f
TaoYang666 <yt635420675@gmail.com>
2020-09-06 22:41:24 -0500

    search

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -44,7 +44,12 @@ bool LinkedList<T>::search(T value) const
 	/** TODO 
 		Fix this method
 	*/
-		
+	while(temp != nullptr){
+		if(temp->getValue() == value){
+			isFound = true;
+		}
+		temp = temp->getNext();
+	}	
 
 	return(isFound);
 }
